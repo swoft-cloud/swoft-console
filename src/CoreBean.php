6f74e283f292e8e727b238b61b755d048f8ed70f
huangzhhui <huangzhwork@gmail.com>
2018-02-03 21:54:03 +0800

    fix interface typo

--- src/CoreBean.php
+++ src/CoreBean.php
@@ -4,14 +4,14 @@ namespace Swoft\Console;
 
 use Swoft\Bean\Annotation\BootBean;
 use Swoft\Console\Router\HandlerMapping;
-use Swoft\Core\BootBeanIntereface;
+use Swoft\Core\BootBeanInterface;
 
 /**
  * The core bean of console
  *
  * @BootBean(server=true)
  */
-class CoreBean implements BootBeanIntereface
+class CoreBean implements BootBeanInterface
 {
     public function beans()
     {
